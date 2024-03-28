189607927c2460c577497deb86a4a14ae1437925
ZiwiiDev <oliverfabian22@gmail.com>
2024-03-28 17:37:55 +0100

    Add project

---
+++ components/main/Skills.tsx
@@ -0,0 +1,27 @@
+import { Frontend_skill } from "@/constants";
+import React from "react";
+import SkillsDataProvider from "../sub/SkillsDataProvider";
+
+const Skills = () => {
+  return (
+    <section
+      id="skills"
+      className="flex flex-col items-center justify-center gap-3 h-full relative overflow-hidden pb-80 py-20"
+      style={{ transform: "scale(0.9" }}
+    >
+      <div className="flex flex-row justify-around flex-wrap mt-4 gap-5 items-center">
+        {Frontend_skill.map((image, index) => (
+          <SkillsDataProvider
+            key={index}
+            src={image.Image}
+            width={image.width}
+            height={image.height}
+            index={index}
+          />
+        ))}
+      </div>
+    </section>
+  );
+};
+
+export default Skills;
